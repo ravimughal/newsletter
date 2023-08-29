910e516c5e2bd9e27e2b7a3453e33d1bffd1ec9f
Ravi Mughal <ravimughal@hotmail.com>
2023-08-28 23:47:23 -0300

    interface

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from tkinter import messagebox
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -6,25 +8,55 @@ from csv_utils import get_emails_from_csv
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
-def main():
-    flow = InstalledAppFlow.from_client_secrets_file('path_to_credentials.json', SCOPES)
-    credentials = flow.run_local_server(port=0)
+class EmailApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Envio de E-mail")
 
-    service = build('gmail', 'v1', credentials=credentials)
+        self.subject_label = tk.Label(root, text="Assunto:")
+        self.subject_label.pack()
 
-    subject = 'Sua Newsletter Semanal'
-    body = 'Olá assinantes! Esta é a nossa última newsletter.'
+        self.subject_entry = tk.Entry(root)
+        self.subject_entry.pack()
 
-    csv_filename = 'lista_assinantes.csv'
-    subscribers = get_emails_from_csv(csv_filename)
+        self.body_label = tk.Label(root, text="Corpo da Mensagem:")
+        self.body_label.pack()
 
-    for subscriber in subscribers:
-        try:
-            sender_email = 'seu_email@example.com'  # Remetente padrão
-            message = create_message(sender_email, subscriber, subject, body)
-            send_message(service, 'me', message)
-        except ValueError as e:
-            print(e)
+        self.body_text = tk.Text(root, height=10, width=40)
+        self.body_text.pack()
 
-if __name__ == '__main__':
-    main()
+        self.send_button = tk.Button(root, text="Enviar E-mail", command=self.send_email)
+        self.send_button.pack()
+
+    def send_email(self):
+        subject = self.subject_entry.get()
+        body = self.body_text.get("1.0", tk.END).strip()
+
+        if subject and body:
+            try:
+                flow = InstalledAppFlow.from_client_secrets_file('path_to_credentials.json', SCOPES)
+                credentials = flow.run_local_server(port=0)
+
+                service = build('gmail', 'v1', credentials=credentials)
+
+                csv_filename = 'lista_assinantes.csv'
+                subscribers = get_emails_from_csv(csv_filename)
+
+                for subscriber in subscribers:
+                    try:
+                        sender_email = 'testedasilvafilho@gmail.com'  # Remetente padrão
+                        message = create_message(sender_email, subscriber, subject, body)
+                        send_message(service, 'me', message)
+                    except ValueError as e:
+                        print(e)
+
+                messagebox.showinfo("Sucesso", "E-mails enviados com sucesso!")
+            except Exception as e:
+                messagebox.showerror("Erro", f"Ocorreu um erro: {e}")
+        else:
+            messagebox.showerror("Erro", "Preencha o assunto e o corpo da mensagem.")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = EmailApp(root)
+    root.mainloop()
